491cd8ce3cc9b0feb68224a208498eeb8ed90cc8
kunal-2308 <kunaltaware210@gmail.com>
2025-01-12 20:25:47 +0530

    Register Controller Fixed : Bugs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,11 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  console.log('Logged Request:', req.body);
-
   let { auth0Id, email, name, picture } = req.body;
 
   if (!auth0Id || !email) {
-    return res.status(400).json({ message: 'auth0Id and email are required' });
+    return res.status(400).json({ message: "auth0Id and email are required" });
   }
 
   if (!name || name.trim().length === 0) {
@@ -24,15 +23,34 @@ const register = async (req, res) => {
         profilePhoto: picture,
       });
       await user.save();
-
-      console.log('User created successfully');
-      return res.status(201).json({ message: 'User registered successfully', user });
+      console.log("User created successfully");
+    } else {
+      console.log("User already exists");
     }
 
-    res.status(200).json({ message: 'User already exists', user });
+    const token = jwt.sign(
+      { auth0Id: user.auth0Id, email: user.email },
+      process.env.SECRET_KEY,
+      { expiresIn: "10d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 10 * 24 * 60 * 60 * 1000,
+    });
+
+    return res
+      .status(user.isNew ? 201 : 200)
+      .json({
+        message: user.isNew
+          ? "User registered successfully"
+          : "User already exists",
+        user,
+      });
   } catch (error) {
-    console.error('Error during registration:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error("Error during registration:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
